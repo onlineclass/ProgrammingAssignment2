9a59cc2bd1043d019d24765d4222d91f2df902cc
onlineclass <coursera.class@rogers.com>
2014-04-15 21:21:41 -0400

    Added a performance test script (to be developed)

---
+++ performanceTest.R
@@ -0,0 +1,4 @@
+## This script executes a simple performance comparison between computing
+## several times in a row the inverse of a (relatively) large matrix using
+## the default R function 'solve' versus the same computation using the
+## cached matrix inverse functions 'makeCacheMatrix' and 'cacheSolve'
